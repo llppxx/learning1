a830fff8837eb092aab774f76b2e4ec6a885e1b7
llppxx <peixuan@meetfire.tech>
2026-07-02 14:22:15 +0800

    增加可视化，以及snapkit约束布局

--- learning1/ViewController.swift
+++ learning1/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
+import SnapKit
+
 class ViewController: UIViewController {
+    
+    let box = UIView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.addSubview(box)
+            box.backgroundColor = .blue
+            
+            box.snp.makeConstraints { make in
+                make.center.equalToSuperview()  //约束位置
+                make.width.height.equalTo(100)  //约束大小
+            }
+        }
 
 }
 
+#if canImport(SwiftUI)
+import SwiftUI
+
+#Preview {
+    ViewController()
+}
+#endif
