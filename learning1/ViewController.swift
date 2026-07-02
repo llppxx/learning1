a9ad03fcf2ca8401277bb95956c291b6a59f09c0
llppxx <peixuan@meetfire.tech>
2026-07-02 17:56:27 +0800

    增加多行输入框

--- learning1/ViewController.swift
+++ learning1/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     let button = UIButton()  //创建按钮
     let imageView = UIImageView()  //创建图片
     let textField = UITextField()  //创建单行输入框
+    let textView = UITextView() //创建多行输入框
     
     @objc func buttonClick() {  //点击按钮方法实现
         print("按钮被点击")
@@ -94,6 +95,25 @@ class ViewController: UIViewController {
             make.top.equalTo(imageView.snp.bottom).offset(40)
             make.width.equalTo(250)
             make.height.equalTo(40)
+        }
+        
+        //多行输入框
+        textView.text = "请输入你的个人简介"
+        textView.font = .systemFont(ofSize: 14)
+        textView.textColor = .black
+        textView.isEditable = true //是否可以编辑
+        textView.isScrollEnabled = true //是否可以滚动
+        textView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        textView.layer.cornerRadius = 10
+        textView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
+        view.addSubview(textView)
+
+        textView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(textField.snp.bottom).offset(40)
+              make.width.equalTo(300)
+              make.height.equalTo(100)
         }
 
     }
