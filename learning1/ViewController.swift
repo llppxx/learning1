8fca73cb48a8dc56a5143adedcedc23774103fed
llppxx <peixuan@meetfire.tech>
2026-07-02 15:11:49 +0800

    显示文本控件

--- learning1/ViewController.swift
+++ learning1/ViewController.swift
@@ -11,16 +11,26 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    let box = UIView()
+    let label = UILabel() //创建
 
-        override func viewDidLoad() {
+    override func viewDidLoad() {
             super.viewDidLoad()
-            view.addSubview(box)
-            box.backgroundColor = .blue
-            
-            box.snp.makeConstraints { make in
-                make.center.equalToSuperview()  //约束位置
-                make.width.height.equalTo(100)  //约束大小
+
+            // 1. 配置属性
+            label.text = "第一个显示文本哈哈哈哈哈"
+            label.textColor = .black
+            label.font = .systemFont(ofSize: 50)
+            label.numberOfLines = 0
+            label.textAlignment = .center
+
+            // 2. 加入视图
+            view.addSubview(label)
+
+            // 3. 布局
+            label.snp.makeConstraints { make in
+                make.top.equalTo(view.safeAreaLayoutGuide).offset(20)//顶部等于安全距离（状态栏），再向下偏移20
+                make.width.equalTo(300) //宽度约束，如果不约束宽度，不会换行
+                make.centerX.equalToSuperview()
             }
         }
 
