523270527324ca635fa8b11c75c628e4c75a4e6c
llppxx <peixuan@meetfire.tech>
2026-07-10 14:54:53 +0800

    给联系人页面增加搜索栏

--- learning1/Home/ViewController.swift
+++ learning1/Home/ViewController.swift
@@ -11,15 +11,37 @@ import SnapKit
 
 class ViewController: UIViewController , UITableViewDataSource ,  UITableViewDelegate  {
     
-    let tableView = UITableView() //创建列表
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "搜索联系人"
+        textField.backgroundColor = .systemGray6
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.leftView = UIView(frame: CGRect(x: 0,y: 0,width: 10,height: 0))
+        textField.leftViewMode = .always
+        return textField
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableViews = UITableView() //创建列表
+        tableViews.dataSource = self  // 设置数据源
+        tableViews.delegate = self
+        tableViews.register(ProfileCell.self, forCellReuseIdentifier: "cell") // 注册cell（自定义）
+        return tableViews
+    }()
+    
+    var filteredUsers = contactList
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+
         navigationItem.title = "通讯录"
         setupAddButton()
-        setupTableView()
+        setupView()
+        setupConstraints()
+        setupTextFiled()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,8 +56,21 @@ class ViewController: UIViewController , UITableViewDataSource ,  UITableViewDel
         print("主页消失啦")
     }
     
+    private func setupTextFiled(){
+        searchTextField.addTarget(self, action: #selector(searchTextChange), for: .editingChanged)
+    }
+    @objc private func searchTextChange(){
+        guard let text = searchTextField.text else {  return  }
+        if text.isEmpty { filteredUsers = contactList }
+        else {
+            filteredUsers = contactList.filter{
+                $0.name.contains(text)
+            }
+        }
+        tableView.reloadData()
+    }
     
-    func setupAddButton(){
+    private func setupAddButton(){
         //系统自动在右上角生成+号，点击后通知当前页面，并调用addContact函数
         let addButton = UIBarButtonItem(barButtonSystemItem:  .add, target: self, action: #selector(addContact))
         
@@ -56,9 +91,18 @@ class ViewController: UIViewController , UITableViewDataSource ,  UITableViewDel
         )
     }
     
-    
-    func setupTableView(){
+    private func setupView(){
+        view.addSubview(searchTextField)
         view.addSubview(tableView)
+    }
+    
+    private func setupConstraints(){
+        
+        searchTextField.snp.makeConstraints{  make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.left.right.equalToSuperview().inset(40)
+            make.height.equalTo(40)
+        }
         
         tableView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -67,20 +111,16 @@ class ViewController: UIViewController , UITableViewDataSource ,  UITableViewDel
             make.height.equalTo(500)
         }
         
-        tableView.dataSource = self  // 设置数据源
-        tableView.delegate = self
-        
-        tableView.register(ProfileCell.self, forCellReuseIdentifier: "cell") // 注册cell（自定义）
     }
     //UITableViewDataSource 协议，必须写
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contactList.count
+        return filteredUsers.count
     }
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ProfileCell
         
-        let model = contactList[indexPath.row]
+        let model = filteredUsers[indexPath.row]
 
         cell.configure(model: model)
         return cell
@@ -88,7 +128,7 @@ class ViewController: UIViewController , UITableViewDataSource ,  UITableViewDel
     
     //UITableViewDelegate协议，规定怎么交互
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let user = contactList[indexPath.row]
+        let user = filteredUsers[indexPath.row]
         let vc = ProfileViewController()
         vc.user = user
         navigationController?.pushViewController(vc, animated: true)
@@ -104,6 +144,7 @@ extension ViewController: AddContactDelegate {
     func didAddContact(_ user: UserModel) {
 
         contactList.append(user)
+        filteredUsers = contactList
         tableView.reloadData()
     }
 }
